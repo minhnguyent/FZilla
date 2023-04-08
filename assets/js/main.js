aef7f0212f27c7246232084b2f7ce238d22ce03c
minhnguyent546 <minhnguyent546@gmail.com>
2023-04-08 23:57:15 +0700

    add sidebar

--- assets/js/main.js
+++ assets/js/main.js
@@ -269,27 +269,182 @@ const hotMovies = [
     },
 ];
 
+const trendingMovies = {
+    day: [
+        {
+            titleEn: 'Avatar: The Way of Water',
+            titleVn: 'Avatar: Dòng Chảy Của Nước',
+            thumb: './assets/img/film/popular/avatar-2.jpg',
+            rating: '7.7',
+            views: '318',
+            year: '2022',
+            trailerUrl: 'https://www.youtube.com/watch?v=o5F8MOz_IDw',
+        },
+        {
+            titleEn: 'Ant-Man and the Wasp: Quantumania',
+            titleVn: 'Người Kiến và Chiến binh Ong: Thế giới Lượng tử',
+            thumb: './assets/img/film/popular/ant-man-3.jpg',
+            rating: '6.4',
+            views: '232',
+            year: '2023',
+            trailerUrl: 'https://www.youtube.com/watch?v=ZlNFpri-Y40',
+        },
+        {
+            titleEn: 'Shazam! Fury of the Gods',
+            titleVn: 'Shazam! Cơn thịnh nộ của các vị thần',
+            thumb: './assets/img/film/popular/shazam-fury-of-the-gods.jpg',
+            rating: '6.5',
+            views: '220',
+            year: '2023',
+            trailerUrl: 'https://www.youtube.com/watch?v=Zi88i4CpHe4',
+        },
+        {
+            titleEn: 'Disenchanted',
+            titleVn: 'Chuyện Thần Tiên Ở New York 2',
+            thumb: './assets/img/film/popular/disenchanted.jpg',
+            rating: '5.6',
+            views: '178',
+            year: '2022',
+            trailerUrl: 'https://www.youtube.com/watch?v=dRuwjZJ-DQw',
+        }
+    ],
+    week: [
+        {
+            titleEn: 'Shang-Chi and the Legend of the Ten Rings',
+            titleVn: 'Shang-Chi Và Huyền Thoại Thập Luân',
+            thumb: './assets/img/film/popular/shang-chi.jpg',
+            rating: '7.4',
+            views: '520',
+            year: '2022',
+            trailerUrl: 'https://www.youtube.com/watch?v=8YjFbMbfXaQ',
+        },
+        {
+            titleEn: 'Loki',
+            titleVn: 'Loki',
+            thumb: './assets/img/film/popular/loki.jpg',
+            rating: '8.2',
+            views: '480',
+            year: '2021',
+            trailerUrl: 'https://www.youtube.com/watch?v=nW948Va-l10',
+        },
+        {
+            titleEn: 'Peaky Blinders (Season 5)',
+            titleVn: 'Bóng ma Anh Quốc (Phần 5)',
+            thumb: './assets/img/film/popular/peaky-blinders-5.jpg',
+            rating: '',
+            views: '213',
+            year: '2019',
+            trailerUrl: 'https://www.youtube.com/watch?v=Ruyl8_PT_y8',
+        },
+        {
+            titleEn: 'Black Adam',
+            titleVn: 'Black Adam',
+            thumb: './assets/img/film/popular/black-adam.jpg',
+            rating: '6.3',
+            views: '202',
+            year: '2022',
+            trailerUrl: 'https://www.youtube.com/watch?v=X0tOpBuYasI',
+        },
+    ],
+    month: [
+        {
+            titleEn: 'Loki',
+            titleVn: 'Loki',
+            thumb: './assets/img/film/popular/loki.jpg',
+            rating: '8.2',
+            views: '1.5k',
+            year: '2021',
+            trailerUrl: 'https://www.youtube.com/watch?v=nW948Va-l10',
+        },
+        {
+            titleEn: 'Peaky Blinders (Season 5)',
+            titleVn: 'Bóng ma Anh Quốc (Phần 5)',
+            thumb: './assets/img/film/popular/peaky-blinders-5.jpg',
+            rating: '',
+            views: '1.2k',
+            year: '2019',
+            trailerUrl: 'https://www.youtube.com/watch?v=Ruyl8_PT_y8',
+        },
+        {
+            titleEn: 'Ant-Man and the Wasp: Quantumania',
+            titleVn: 'Người Kiến và Chiến binh Ong: Thế giới Lượng tử',
+            thumb: './assets/img/film/popular/ant-man-3.jpg',
+            rating: '6.4',
+            views: '920',
+            year: '2023',
+            trailerUrl: 'https://www.youtube.com/watch?v=ZlNFpri-Y40',
+        },
+        {
+            titleEn: 'Shazam! Fury of the Gods',
+            titleVn: 'Shazam! Cơn thịnh nộ của các vị thần',
+            thumb: './assets/img/film/popular/shazam-fury-of-the-gods.jpg',
+            rating: '6.5',
+            views: '678',
+            year: '2023',
+            trailerUrl: 'https://www.youtube.com/watch?v=Zi88i4CpHe4',
+        },
+    ]
+};
+
+const inTrailerMovies = [
+    {
+        titleEn: 'Avatar: The Way of Water',
+        titleVn: 'Avatar: Dòng Chảy Của Nước',
+        thumb: './assets/img/film/trailer/avatar-2.jpg',
+        year: '2022',
+        views: '2.6k',
+        likes: '928',
+        trailerUrl: 'https://www.youtube.com/watch?v=o5F8MOz_IDw',
+    },
+    {
+        titleEn: 'Shazam! Fury of the Gods',
+        titleVn: 'Shazam! Cơn thịnh nộ của các vị thần',
+        thumb: './assets/img/film/trailer/shazam-fury-of-the-gods.jpg',
+        year: '2023',
+        views: '1.2k',
+        likes: '500',
+        trailerUrl: 'https://www.youtube.com/watch?v=Zi88i4CpHe4',
+    },
+    {
+        titleEn: 'John Wick: Chapter 4',
+        titleVn: 'John wick: Chương 4',
+        thumb: './assets/img/film/trailer/john-wick-4.jpg',
+        year: '2023',
+        views: '860',
+        likes: '335',
+        trailerUrl: 'https://www.youtube.com/watch?v=yjRHZEUamCc',
+    },
+    {
+        titleEn: 'Guy Ritchie’s The Covenant',
+        titleVn: 'Khế Ước',
+        thumb: './assets/img/film/trailer/the-covenant.jpg',
+        year: '2023',
+        views: '520',
+        likes: '128',
+        trailerUrl: 'https://www.youtube.com/watch?v=02PPMPArNEQ',
+    },
+];
+
 const renderPopularMovies = function(root) {
     if (!root) return; 
 
-    root.innerHTML = '';
-    popularMovies.forEach((movie) => {
-        const child = `
+    root.innerHTML = popularMovies.map((movie) => {
+        return `
             <div class="swiper-slide main-section__carousel-slide">
                 <div class="film-item">
                     <div class="film-item-inner">
                         <div class="film-item__main">
                             <div class="film-item__thumb">
-                            <img src="${movie.thumb}" alt="${movie.titleEn}" class="film-item__thumb-img">
+                                <img src="${movie.thumb}" alt="${movie.titleEn}" class="film-item__thumb-img">
                             </div>
 
                             <div class="film-item__label">
-                            <div class="film-item__label-item">HD</div>
-                            <div class="film-item__label-item">Thuyết minh</div>
+                                <div class="film-item__label-item">HD</div>
+                                <div class="film-item__label-item">Thuyết minh</div>
                             </div>
 
                             <div class="film-item__bookmark" title="Bookmark">
-                            <span class="film-item__bookmark-icon"><i class="fa-regular fa-bookmark"></i></span>
+                                <span class="film-item__bookmark-icon"><i class="fa-regular fa-bookmark"></i></span>
                             </div>
 
                             <div class="film-item__rating ${movie.rating ? '' : 'film-item__rating--not-available'}">
@@ -312,27 +467,25 @@ const renderPopularMovies = function(root) {
                 </div>
             </div>
         `;
-        root.innerHTML += child;
-    });
+    }).join('');
 }
 
 const renderMovieList = (root, movies) => {
     if (!root || !movies) return;
 
-    root.innerHTML = '';
-    movies.forEach((movie) => {
-        const child = `
+    root.innerHTML = movies.map((movie) => {
+        return `
             <div class="col-3">
                 <div class="film-item">
                     <div class="film-item-inner">
                         <div class="film-item__main">
                             <div class="film-item__thumb">
-                            <img src="${movie.thumb}" alt="${movie.titleEn}" class="film-item__thumb-img">
+                                <img src="${movie.thumb}" alt="${movie.titleEn}" class="film-item__thumb-img">
                             </div>
 
                             <div class="film-item__label">
-                            <div class="film-item__label-item">HD</div>
-                            <div class="film-item__label-item">Thuyết minh</div>
+                                <div class="film-item__label-item">HD</div>
+                                <div class="film-item__label-item">Thuyết minh</div>
                             </div>
 
                             <div class="film-item__bookmark" title="Bookmark">
@@ -359,17 +512,82 @@ const renderMovieList = (root, movies) => {
                 </div>
             </div>
         `;
-        root.innerHTML += child;
-    });
+    }).join('');
+}
+
+const renderTrailers = (root, movies) => {
+    if (!root || !movies) return;
+
+    root.innerHTML = movies.map((movie) => {
+        return `
+            <div class="swiper-slide main-sidebar-section__carousel-slide">
+                <div class="trailer-item">
+                    <div class="trailer-item-inner">
+                        <div class="trailer-item__main">
+                            <div class="trailer-item__thumb">
+                                <img src="${movie.thumb}" class="trailer-item__thumb-img" alt="${movie.titleVn}"></img>
+                            </div>
+
+                            <div class="trailer-item__label">Trailer</div>
+                        </div>
+
+                        <div class="trailer-item__desc">
+                            <div class="trailer-item__title">${movie.titleVn} - ${movie.titleEn} (${movie.year})</div>
+
+                            <div class="trailer-item__views">
+                                <span class="trailer-item__views-icon"><i class="fa-solid fa-eye"></i></span>
+                                <span class="trailer-item__views-quantity">${movie.views}</span>
+                            </div>
+                            <div class="trailer-item__like">
+                                <span class="trailer-item__like-icon"><i class="fa-solid fa-thumbs-up"></i></span>
+                                <span class="trailer-item__like-quantity">${movie.likes}</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `;
+    }).join('');
+}
+
+const renderTrendingMovies = (root, movies) => {
+    if (!root || !movies) return;
+
+    root.innerHTML = movies.map((movie) => {
+        return `
+            <div class="main-sidebar-section__tab-pane-item">
+                <div class="main-sidebar-section__tab-pane-item-thumb">
+                    <img src="${movie.thumb}" class="main-sidebar-section__tab-pane-item-img" alt="${movie.titleVn}">
+                </div>
+                <div class="main-sidebar-section__tab-pane-item-desc">
+                    <div class="main-sidebar-section__tab-pane-item-title">${movie.titleVn}</div>
+                    <div class="main-sidebar-section__tab-pane-item-subtitle">${movie.year}</div>
+
+                    <div class="main-sidebar-section__tab-pane-item-views">
+                        <span>${movie.views}</span> lượt người xem
+                    </div>
+                </div>
+            </div>
+        `;
+    }).join('');
 }
 
 renderPopularMovies($('.main-section__carousel-wrapper'));
+
 renderMovieList($('.main-section--category-1 .main-section__body .row'), hotMovies);
 renderMovieList($('.main-section--category-2 .main-section__body .row'), hotMovies);
 renderMovieList($('.main-section--category-3 .main-section__body .row'), hotMovies);
 renderMovieList($('.main-section--category-4 .main-section__body .row'), hotMovies);
 
-const swiper = new Swiper('.main-section__carousel', {
+renderTrailers($('.main-sidebar-section__carousel-wrapper'), inTrailerMovies);
+
+renderTrendingMovies($('.main-sidebar-section__tab-pane--day'), trendingMovies.day);
+renderTrendingMovies($('.main-sidebar-section__tab-pane--week'), trendingMovies.week);
+renderTrendingMovies($('.main-sidebar-section__tab-pane--month'), trendingMovies.month);
+
+
+
+const mainSectionCarousel = new Swiper('.main-section__carousel', {
     autoplay: {
         delay: 4000,
         pauseOnMouseEnter: true,
@@ -385,13 +603,53 @@ const swiper = new Swiper('.main-section__carousel', {
         // TODO
     },
 
-    speed: 1200,
-    mousewheelControl: 'true',
-    // Navigation arrows
+    speed: 1300,
     navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+        nextEl: '.main-section__carousel-btn-next',
+        prevEl: '.main-section__carousel-btn-prev',
     },
+});
+
+const mainSidebarSectionCarousel = new Swiper('.main-sidebar-section__carousel', {
+    autoplay: {
+        delay: 4000,
+        pauseOnMouseEnter: true,
+        disableOnInteraction: false
+    },
+    spaceBetween: 30,
+    effect: 'fade',
+    fadeEffect: { crossFade: true },
+    loop: true,
+    breakpoints: {
+        // TODO
+    },
+    speed: 600,
+    pagination: {
+        el: ".main-sidebar-section__carousel-pagination",
+        type: "progressbar",
+    },
+    navigation: {
+        nextEl: '.main-sidebar-section__carousel-btn-next',
+        prevEl: '.main-sidebar-section__carousel-btn-prev',
+    },
+});
+
+// main-sidebar-section__tabs
+const tabItems = $$('.main-sidebar-section__tab-item');
+const tabPanes = $$('.main-sidebar-section__tab-pane');
+const tabLine = $('.main-sidebar-section__tab-line');
+
+tabItems.forEach((tabItem, index) => {
+    tabItem.onclick = () => {
+        $('.main-sidebar-section__tab-item.active')?.classList.remove('active');
+        $('.main-sidebar-section__tab-pane.active')?.classList.remove('active');
+
+        tabItem.classList.add('active');
+        tabPanes[index].classList.add('active');
+
+        tabLine.style.left = tabItem.offsetLeft + 'px';
+        tabLine.style.width = tabItem.offsetWidth + 'px';
+    }
 });
 
 // to-top btn
